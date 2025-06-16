bbd0775e885b7fac84d189df69beac7bedff21bc
Patrick <207300137+PandasAreSus@users.noreply.github.com>
2025-06-16 03:46:38 -0400

    Reworked to have a gui rather than being play in console.log

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+let roundCount = 0;
 
 function getComputerChoice() {
     const seed = Math.random();
@@ -12,52 +14,96 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() { 
-    const choice = String(prompt("Choose rock, paper, or scissors"));    
-    return choice.toLowerCase();
+function setHumanChoice(input) {
+    humanChoice = input;
 }
 
-/*
-    9 RPS cominations:
-    3 Equal
-    2 P > R, R < P
-    2 R > S, S < R
-    2 S > P, P < S
+const results = document.querySelector("#results");
+const btnR = document.querySelector("#btnR");
+const btnP = document.querySelector("#btnP");
+const btnS = document.querySelector("#btnS");
+const instruct = document.querySelector("#instruct");
+const pChoice = document.querySelector("#pChoice");
+const cChoice = document.querySelector("#cChoice");
+const roundCt = document.querySelector("#roundCt");
+const humScore = document.querySelector("#humScore");
+const comScore = document.querySelector("#comScore");
 
-*/
+btnR.addEventListener("click", () => {
+    setHumanChoice("rock");
+    playRound(humanChoice, getComputerChoice())
+});
 
+btnP.addEventListener("click", () => {
+    setHumanChoice("paper");
+    playRound(humanChoice, getComputerChoice())
+})
 
-function playGame() {
-    let humanSelection;
-    let computerSelection;
+btnS.addEventListener("click", () => {
+    setHumanChoice("scissors");
+    playRound(humanChoice, getComputerChoice())
+})
 
-    function playRound(humanChoice, computerChoice) {
-    switch(computerChoice){
-        case "rock":
-            if (humanChoice === "paper") humanScore++;
-            if (humanChoice === "scissors") computerScore++;
-            break;
-        case "paper":
-            if (humanChoice === "rock") computerScore++;
-            if (humanChoice === "scissors") humanScore++;
-            break;
-        case "scissors":
-            if (humanChoice === "paper") computerScore++;
-            if (humanChoice === "rock") humanScore++;
-            break;    
-    }    
+function playRound(humanChoice, computerChoice) {
+    instruct.style.color = "white";
+    if (roundCount < 5) {
+        switch(computerChoice){ 
+            case "rock":
+                if (humanChoice === "paper") {
+                    humanScore++; 
+                    instruct.textContent = "You won the round, choose again!";
+                } else if (humanChoice === "scissors") {
+                    computerScore++; 
+                    instruct.textContent = "You lost the round, choose again"; 
+                } else {
+                    instruct.textContent = "You tied, choose again!";
+                }
+                break;
+            case "paper":
+                if (humanChoice === "rock") {
+                    computerScore++; 
+                    instruct.textContent = "You lost the round, choose again";
+                } else if (humanChoice === "scissors") {
+                    humanScore++; 
+                    instruct.textContent = "You won the round, choose again!";
+                } else {
+                    instruct.textContent = "You tied, choose again!";
+                }
+                break;
+            case "scissors":
+                if (humanChoice === "paper") {
+                    computerScore++; 
+                    instruct.textContent = "You lost the round, choose again";
+                } else if (humanChoice === "rock") {
+                    humanScore++; 
+                    instruct.textContent = "You won the round, choose again!";
+                } else {
+                    instruct.textContent = "You tied, choose again!";
+                }
+                break;    
+        } 
+
+        roundCount++;
+        roundCt.textContent = "Round: " + roundCount;
+        humScore.textContent = "Human: " + humanScore;
+        comScore.textContent = "Computer: " + computerScore;
+        pChoice.textContent = humanChoice;
+        cChoice.textContent = computerChoice;
     }
 
-    for (let i = 0; i < 5; i++) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
+    if (roundCount == 5) {
+        if (humanScore > computerScore) {
+            instruct.style.color = "rgb(0, 195, 255)"
+            instruct.textContent = "You won the game! Select another to play again!"
+        } else {
+            instruct.style.color = "red";
+            instruct.textContent = "You lost the game! Select another to play again!"
+        }
 
-        playRound(humanSelection, computerSelection);
-
-        console.log("Human chose: " + humanSelection + ", computer chose: " + computerSelection);
-        console.log("Human: " + humanScore + " Computer: " + computerScore);
+        roundCount = 0;
+        humanScore = 0;
+        computerScore = 0;
     }
-
 }
 
-playGame();
+
